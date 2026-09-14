8aeef0ce2e2cd394d28a482f444daf03609fd84f
Raphael Nabet <nabetraphael@icloud.com>
2026-09-14 10:09:47 +0200

    chore(display): add display script

---
+++ src/utils/progress_bar.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+draw_progress_bar() {
+    local progress=$1
+    local total=$2
+    local width=50
+    
+    local percentage=$((progress * 100 / total))
+    local completed=$((progress * width / total))
+    local remaining=$((width - completed))
+    
+    local bar=$(printf "%0.s#" $(seq 1 $completed))
+    local spaces=$(printf "%0.s-" $(seq 1 $remaining))
+    
+    printf "\r[%s%s] %d%%  (%d/%d)\n" "$bar" "$spaces" "$percentage" "$progress" "$total"
+}
